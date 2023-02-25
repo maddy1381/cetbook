1afac7308ef585539a8ed50069f05aa85f7bf30e
Madhav Sharma <ms1381598@gmail.com>
2023-02-25 20:15:52 +0530

    first commit

---
+++ src/main/java/com/cetbook/CetBook/CetBookApplication.java
@@ -0,0 +1,14 @@
+package com.cetbook.CetBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CetBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CetBookApplication.class, args);
+		System.out.println("cetbook");
+	}
+
+}
---
+++ src/test/java/com/cetbook/CetBook/CetBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cetbook.CetBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CetBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
